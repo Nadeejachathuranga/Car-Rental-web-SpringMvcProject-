7b8bafc1d124efd88bb22043eafa1c47a46ed411
nadeeja <nadeeja20@gmail.com>
2024-02-08 18:28:29 +0530

    commit

--- FrontEnd/controller/generalCarSelectingController.js
+++ FrontEnd/controller/generalCarSelectingController.js
@@ -2,17 +2,60 @@ const BASE_URL = "http://localhost:8080/BackEnd_war";
 
 var carid;
 
-function findGeneralCars(){
+// pick up days calculate value
+var year="";
+var month="";
+var day="";
+var pickupDay;
+
+// total rent days calculate value
+var rTDays;
+
+
+
+///////////////////////////////////////////////////
+// set pick up year functions
+$("#yearSelect").click(function (){
+    year= $("#yearSelect").val()
+    pickupDay=year+"/"+month+"/"+day;
+    document.getElementById("pickupday").innerHTML=pickupDay;
+})
+
+// set pick up month functions
+$("#monthSelect").click(function (){
+    month= $("#monthSelect").val()
+    pickupDay=year+"/"+month+"/"+day;
+    document.getElementById("pickupday").innerHTML=pickupDay;
+})
+
+// set pick up day functions
+$("#daySelect").click(function (){
+    day= $("#daySelect").val()
+    pickupDay=year+"/"+month+"/"+day;
+    document.getElementById("pickupday").innerHTML=pickupDay;
+  //  pickDateValidation(pickupDay,carid);
+})
+
+
+///////////////////////////////////////////////////
+// set total rent days functions
+$("#daysSelect").click(function (){
+    rTDays=$("#daysSelect").val()
+    document.getElementById("rentdays").innerHTML=rTDays+" days";
+})
+
+
+function findGeneralCars() {
     $("#tblCustomer").empty();
     $.ajax({
-        url:BASE_URL+"/car",
-        method:'GET',
-        success:function (resp) {
+        url: BASE_URL + "/car",
+        method: 'GET',
+        success: function (resp) {
             let cars = resp.data;
             for (let i in cars) {
                 let car = cars[i];
-                if (car.type=="GENERAL") {
-                      carid=car.id;
+                if (car.type == "GENERAL") {
+                    carid = car.id;
                     let name = car.name;
                     let brand = car.brand;
                     let colour = car.color;
@@ -27,11 +70,11 @@ function findGeneralCars(){
             }
             selectRow();
         },
-        error:function (error) {
+        error: function (error) {
             alert("error");
         }
     })
-
+}
     function selectRow() {
         let name;
         $('#tblCustomer>tr').click(function () {
@@ -41,4 +84,59 @@ function findGeneralCars(){
             document.getElementById("carName").innerHTML = name;
         })
     }
+
+ $("#conform").click(function () {
+     var userId=$("#idInputTxt").val()
+     rentRequest(userId);
+ })
+
+function rentRequest(userId){
+    let id="rent115";
+    let status="pending";
+    $.ajax({
+        url: BASE_URL + "/rent?rentId="+id+"&cusId="+userId+"&carId="+carId+"&days="+rTDays+"&pickDay="+pickupDay+"&statuss="+status,
+        method: 'POST',
+        success: function (response) {
+            let res=response.state
+            if (res=="Ok"){
+                alert("Request send successfully");
+            }
+        },
+        error: function (error) {
+            alert("Something went wrong");
+        }
+    });
+};
+
+function pickDateValidation(pickupDay){
+    $.ajax({
+        url: BASE_URL + "/rent",
+        method: 'GET',
+        success: function (resp) {
+            let res=resp.data;
+            for (let i in res) {
+                let rent = res[i];
+                if (rent.statuss=="Accept"){
+                    if (rent.pickDay==pickupDay){
+                        document.getElementById("errorMsg").innerHTML="Sorry this date already book";
+                        document.getElementById("conform").disabled = true;
+                    }else {
+                        document.getElementById("errorMsg").innerHTML="";
+                        document.getElementById("conform").disabled = false;
+                    }
+                }
+             /*   if(rent.carId == "car002") {
+                   alert(carid)
+                   if (rent.statuss == "Accept") {
+                       document.getElementById("errorMsg").innerHTML = "found";
+                   }else {
+                       document.getElementById("errorMsg").innerHTML = "cant find";
+                   }
+                }*/
+            }
+        },
+        error:function (error) {
+            alert("Something went wrong");
+        },
+    });
 }
